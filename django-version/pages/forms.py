88ff9a465186ac6423cabd13f3305dd2af412913
Ibrahim Michael <segunmichael24@gmail.com>
2023-09-30 01:41:56 +0100

    form correct and template update

--- django-version/pages/forms.py
+++ django-version/pages/forms.py
@@ -4,6 +4,6 @@ class ContactForm(forms.Form):
    full_name = forms.CharField(max_length = 150, 
                               widget= forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter name'}))
    phone_number = forms.CharField(max_length = 13,
-                                  widget= forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter name'}))
+                                  widget= forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter phone number'}))
    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Enter email'}))
    message = forms.CharField(max_length=250, widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Enter message'}))
